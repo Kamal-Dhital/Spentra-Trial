4ce883fc517565e98bfe470dd27fad9a11051efb
Kamal Dhital <kdhital143@gmail.com>
2025-04-26 17:20:44 +0545

    Add expenses functionality

--- backend/spentra/urls.py
+++ backend/spentra/urls.py
@@ -19,4 +19,5 @@ urlpatterns = [
     path('api/auth/', include('authentication.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/', include('income.urls')),
+    path('api/expenses/', include('expenses.urls')),
 ]
